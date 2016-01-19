0f080d6656bfc854a4af2863a8ff54be7e3b248e
maximlarionov <maxim.larionov@flatstack.com>
2016-01-19 17:27:09 +0300

    Add mail sender

--- site.rb
+++ site.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
   File.read(File.join(settings.public_folder, 'index.html'))
@@ -30,4 +31,25 @@ end
 
 get '/en-contacts.html' do
   File.read(File.join(settings.public_folder, 'en-contacts.html'))
+end
+
+post '/sendmail' do
+  Pony.mail(
+    :to => params[:email],
+    :from => params[:email],
+    :subject => "Hello, #{params[:name]} filled the form on ADEKO site!",
+    :body => "#{params[:name]} said: \n
+              #{params[:message]} \n
+              Contacts: \n
+                Phone: #{params[:phone]} \n
+                Company: #{params[:company]}",
+
+    :via => :sendmail,
+    :via_options => {
+      :location  => '/usr/sbin/sendmail', # defaults to 'which sendmail' or '/usr/sbin/sendmail' if 'which' fails
+      :arguments => '-t' # -t and -i are the defaults
+    }
+  )
+
+  redirect back
 end
